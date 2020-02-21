7ea5dba47356892f1703e017b6c49f8e8f64f45c
joyja <joyja@latitude1>
2020-02-21 19:18:22 +0000

    added getSchema.js and schema.json

---
+++ getSchema.js
@@ -0,0 +1,44 @@
+const fs = require('fs')
+const fetch = require('node-fetch')
+
+const url = process.env.TENTACLE_URL || 'http://localhost:4000'
+
+const getSchema = function() {
+  return fetch(url, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      variables: {},
+      query: `
+        {
+          __schema {
+            types {
+              kind
+              name
+              possibleTypes {
+                name
+              }
+            }
+          }
+        }
+      `
+    })
+  })
+    .then((result) => result.json())
+    .then((result) => {
+      // here we're filtering out any type information unrelated to unions or interfaces
+      const filteredData = result.data.__schema.types.filter(
+        (type) => type.possibleTypes !== null
+      )
+      result.data.__schema.types = filteredData
+      fs.writeFileSync('./schema.json', JSON.stringify(result.data), (err) => {
+        if (err) {
+          console.error('Error writing fragmentTypes file', err)
+        } else {
+          console.log('Fragment types successfully extracted!')
+        }
+      })
+    })
+}
+
+module.exports = getSchema
