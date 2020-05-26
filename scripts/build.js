1d2922d75bd6e6ead4b20cc211896e95f825d1d6
joyja <joyja@latitude1>
2020-05-26 21:30:03 +0000

    changed rootDir on build & start

--- scripts/build.js
+++ scripts/build.js
@@ -1,8 +1,12 @@
 #!/usr/bin/env node
+const path = require('path')
 const { loadNuxt, build } = require('nuxt')
 
 const main = async () => {
-  const nuxt = await loadNuxt({ for: 'build' })
+  const nuxt = await loadNuxt({
+    rootDir: path.join(__dirname, '../'),
+    for: 'build',
+  })
   await build(nuxt)
 }
 
--- scripts/start.js
+++ scripts/start.js
@@ -1,8 +1,12 @@
 #!/usr/bin/env node
+const path = require('path')
 const { loadNuxt } = require('nuxt-start')
 
 const main = async () => {
-  const nuxt = await loadNuxt({ for: 'start' })
+  const nuxt = await loadNuxt({
+    rootDir: path.join(__dirname, '../'),
+    for: 'start',
+  })
   await nuxt.listen(3000)
 }
 
