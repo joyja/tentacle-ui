608c962fccb8858bb354f0f1406d8dc1c0310f4e
joyja <joyja@latitude1>
2020-05-28 03:02:06 +0000

    fixed window error on server from apollo config

--- plugins/apollo-config.js
+++ plugins/apollo-config.js
@@ -8,21 +8,24 @@ const fragmentMatcher = new IntrospectionFragmentMatcher({
 })
 
 export default function (context) {
-  const clientConfig = {
-    httpPrefix: process.env.tentacleClientSecure ? 'https' : 'http',
-    wsPrefix: process.env.tentacleClientSecure ? 'wss' : 'ws',
-    hostname: process.env.tentacleClientHost || window.location.hostname,
-    port: process.env.tentacleClientPort || '80',
-    url: process.env.tentacleClientUrl || '/api/',
+  let config = {}
+  if (process.client) {
+    config = {
+      httpPrefix: process.env.tentacleClientSecure ? 'https' : 'http',
+      wsPrefix: process.env.tentacleClientSecure ? 'wss' : 'ws',
+      hostname: process.env.tentacleClientHost || window.location.hostname,
+      port: process.env.tentacleClientPort || '80',
+      url: process.env.tentacleClientUrl || '/api/',
+    }
+  } else {
+    config = {
+      httpPrefix: process.env.tentacleServerSecure ? 'https' : 'http',
+      wsPrefix: process.env.tentacleServerSecure ? 'wss' : 'ws',
+      hostname: process.env.tentacleServerHost || 'localhost',
+      port: process.env.tentacleServerPort || 4000,
+      url: process.env.tentacleServerUrl || '/',
+    }
   }
-  const serverConfig = {
-    httpPrefix: process.env.tentacleServerSecure ? 'https' : 'http',
-    wsPrefix: process.env.tentacleServerSecure ? 'wss' : 'ws',
-    hostname: process.env.tentacleServerHost || 'localhost',
-    port: process.env.tentacleServerPort || 4000,
-    url: process.env.tentacleServerUrl || '/',
-  }
-  const config = process.client ? clientConfig : serverConfig
   return {
     httpEndpoint: `${config.httpPrefix}://${config.hostname}:${config.port}/${config.url}`,
     wsEndpoint: `${config.wsPrefix}://${config.hostname}:${config.port}/${config.url}`,
